9d861bcb2bfd716e3135a22c2d366931a42844ad
Vladimir Davidovich <thy.ringo@gmail.com>
2026-01-30 10:40:33 +0300

    why rename only in .d.ts?

--- src/NodeApi.Generator/TypeDefinitionsGenerator.cs
+++ src/NodeApi.Generator/TypeDefinitionsGenerator.cs
@@ -2503,66 +2503,6 @@ type DateTime = Date & { kind?: 'utc' | 'local' | 'unspecified' }
 
     private static string TSIdentifier(string? identifier)
     {
-        return string.IsNullOrEmpty(identifier) ? "_" :
-            s_tsReservedWords.Contains(identifier!) ? "_" + identifier : identifier!;
+        return string.IsNullOrEmpty(identifier) ? "_" : identifier!;
     }
-
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Lexical_grammar#reserved_words
-    private static readonly HashSet<string> s_tsReservedWords = new()
-    {
-        "arguments",
-        "as",
-        "async",
-        "await",
-        "break",
-        "case",
-        "catch",
-        "class",
-        "const",
-        "continue",
-        "debugger",
-        "default",
-        "delete",
-        "do",
-        "else",
-        "enum",
-        "eval",
-        "export",
-        "extends",
-        "false",
-        "finally",
-        "for",
-        "from",
-        "function",
-        "get",
-        "if",
-        "implements",
-        "import",
-        "in",
-        "instanceof",
-        "interface",
-        "let",
-        "new",
-        "null",
-        "of",
-        "package",
-        "private",
-        "protected",
-        "public",
-        "return",
-        "set",
-        "static",
-        "super",
-        "switch",
-        "this",
-        "throw",
-        "true",
-        "try",
-        "typeof",
-        "var",
-        "void",
-        "while",
-        "with",
-        "yield",
-    };
 }
